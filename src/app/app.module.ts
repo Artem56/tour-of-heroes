af1e54ea3137164c5f7fc266a4826fc0087f55ff
artem <artemsolomatin19@mail.ru>
2017-11-05 01:59:23 +0300

    Компонент hero вынесен в отдельный файл

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule } from '@angular/forms'; // <-- NgModel is here
 
 import { AppComponent } from './app.component';
-import {HeroDetailComponent} from './hero-detail.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
   imports: [
@@ -13,6 +15,10 @@ import {HeroDetailComponent} from './hero-detail.component';
   declarations: [
     AppComponent,
     HeroDetailComponent,
+    HeroesComponent
+  ],
+  providers: [
+    HeroService
   ],
   bootstrap: [ AppComponent ]
 })
