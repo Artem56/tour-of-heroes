3f58e67806a9b48a049f40a7b873f892fa85d742
artem <artemsolomatin19@mail.ru>
2017-11-05 21:12:34 +0300

    Маршрутизация вынесена в отдельный файл

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,49 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; // <-- NgModel is here
-import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
-import {DashboardComponent} from './dashboard.component';
 
-
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
-    RouterModule.forRoot([
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {                       // redirect
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'detail/:id',
-        component: HeroDetailComponent
-      },
-    ])
+    FormsModule,
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent,
-    HeroesComponent,
     DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
   ],
-  providers: [
-    HeroService
-  ],
+  providers: [ HeroService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
